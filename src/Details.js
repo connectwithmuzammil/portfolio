629abce1e99a9f920d7e63d7af9ff9a32368808b
muzammilmmk <muzammilmmk.77@gmail.com>
2024-05-16 00:56:15 +0500

    updated code

---
+++ src/Details.js
@@ -0,0 +1,173 @@
+// Enter all your detials in this file
+// Logo images
+import logoDark from "./assets/logoDark.svg";
+import logoWhite from "./assets/logoWhite.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logoDark: logoDark,
+  logoWhite: logoWhite,
+
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "M Muzammil Khan",
+  tagline: "a passionate front-end developer",
+  img: profile,
+  about: `As a front-end developer with over a year's experience, I specialize in React JS, Next JS. I've excelled in fast-paced team environments, delivering high-quality software solutions and captivating user interfaces. My diverse experience includes working on ReactJS, and Next JS Technologies. Passionate about staying updated with front-end trends, I strive for continuous improvement and innovation in my work.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/connectwithmuzammil/",
+  github: "https://github.com/connectwithmuzammil",
+  twitter: "https://twitter.com/Muzammilmmk77",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Pluton PVT LTD`,
+    Location: "Karachi,Pakistan",
+    Type: "Full Time",
+    Duration: "Dec 2023 - Present ",
+  },
+  {
+    Position: "Junior Software Engineer",
+    Company: `Strugbits PVT LTD`,
+    Location: "Karachi,Pakistan",
+    Type: "Full Time",
+    Duration: "May 2023 - Sep 2023",
+  },
+  {
+    Position: "Internship",
+    Company: `Strugbits PVT LTD`,
+    Location: "Karachi,Pakistan",
+    Type: "Internship",
+    Duration: "Mar 2023 - May 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Dha Suffa University",
+    Company: "BS (Computer Science)",
+    Location: "Onsite",
+    Type: "Full Time",
+    Duration: "Sep 2018 - Feb 2023",
+  },
+  // {
+  //   Position: "Bachelor in Electronics & Communication",
+  //   Company: `Your College Name here`,
+  //   Location: "Bengaluru",
+  //   Type: "Full Time",
+  //   Duration: "Aug 2020 - Present",
+  // },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  // sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Eccomerce Espresso",
+    image: projectImage1,
+    description: `Designed and developed a user-friendly UI for an e-commerce website. Integrated REST APIs for seamless communication and streamlined functionality. Implemented secure Stripe payment method for smooth transactions`,
+    techstack: "React Js, React Query ",
+    previewLink: "https://menu.javatimescaffe.com/",
+    githubLink: "https://github.com/connectwithmuzammil",
+  },
+  {
+    title: "Pawjamas",
+    image: projectImage2,
+    description: `Transformed Figma designs into sleek and responsive React.js user interfaces, seamlessly translating visual concepts into interactive web experiences.`,
+    techstack: "HTML/CSS, React JS",
+    previewLink: "https://pawjamas.pluton.ltd/",
+    githubLink: "https://github.com/connectwithmuzammil",
+  },
+  {
+    title: "Open Sea Clone",
+    image: projectImage3,
+    description: `Crafted a clone of OpenSea using React.js and Styled Components`,
+    techstack: "React Js, Styled Component  ",
+    previewLink: "https://opensea-nft-clone.netlify.app/",
+    githubLink: "https://github.com/connectwithmuzammil",
+  },
+  {
+    title: "Coffee Cart",
+    image: projectImage4,
+    description: `Designed and developed an e-commerce application using React.js , enabling users to effortlessly browse, purchase, and manage coffee beans machines with intuitive CRUD operations, ensuring a seamless shopping experience`,
+    techstack: "React Js, React Query ",
+    previewLink: "https://shop.javatimescaffe.com/",
+    githubLink: "https://github.com/connectwithmuzammil",
+  },
+  //   {
+  //     title: "Project title 5",
+  //     image: projectImage5,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: "HTML/CSS, JavaScript",
+  //     previewLink: "https://google.com",
+  //     githubLink: "https://github.com/connectwithmuzammil",
+  //   },
+  //   {
+  //     title: "Project title 6",
+  //     image: projectImage6,
+  //     description: `This is sample project description random things are here in description This is sample
+  // project lorem ipsum generator for dummy content`,
+  //     techstack: "HTML/CSS, JavaScript",
+  //     previewLink: "https://google.com",
+  //     githubLink: "https://github.com/connectwithmuzammil",
+  //   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "muzammilmmk.77@gmail.com",
+  phone: "+92 331 1391150",
+};
